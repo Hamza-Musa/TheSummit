22cee1dfb89ed2b76cfcf4d5acebc90a246f5aad
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2023-11-06 22:47:28 +0000

    Update Multipy.js

--- Multipy.js
+++ Multipy.js
@@ -1,3 +1,9 @@
-function multiply(a, b) {
-  return a * b;
-}
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+
+function multiply(a, b){
+    return a * b
+  }
