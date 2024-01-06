2fce5056b333ef9eb71a693b8ac284c20ef63aa1
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-01-06 17:26:28 +0000

    Add coding challenge for 06-01-2024.

---
+++ Function2Squaring.js
@@ -0,0 +1,12 @@
+/* Instructions: 
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// Answer
+const square = function (n1) {
+  return n1 * n1;
+};
+
+// function square(n1){
+//   return n1*n1
+// }
