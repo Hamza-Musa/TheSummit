9beab1c234909cd637044a00c96b3f59817a48c3
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2024-01-25 11:45:50 +0000

    Update CenturyfromYear.js

--- CenturyfromYear.js
+++ CenturyfromYear.js
@@ -13,51 +13,11 @@ Examples
 
 //My answer
 function century(year) {
-  if (year <= 100) {
-    return 1;
-  } else if (year <= 200) {
-    return 2;
-  } else if (year <= 300) {
-    return 3;
-  } else if (year <= 400) {
-    return 4;
-  } else if (year <= 500) {
-    return 5;
-  } else if (year <= 600) {
-    return 6;
-  } else if (year <= 700) {
-    return 7;
-  } else if (year <= 800) {
-    return 8;
-  } else if (year <= 900) {
-    return 9;
-  } else if (year <= 1000) {
-    return 10;
-  } else if (year <= 1100) {
-    return 11;
-  } else if (year <= 1200) {
-    return 12;
-  } else if (year <= 1300) {
-    return 13;
-  } else if (year <= 1400) {
-    return 14;
-  } else if (year <= 1500) {
-    return 15;
-  } else if (year <= 1600) {
-    return 16;
-  } else if (year <= 1700) {
-    return 17;
-  } else if (year <= 1800) {
-    return 18;
-  } else if (year <= 1900) {
-    return 19;
-  } else if (year <= 2000) {
-    return 20;
-  } else if (year <= 2100) {
-    return 21;
-  } else {
-    return 22;
-  }
+  // Calculate the century by dividing the year by 100 and rounding up
+  return Math.ceil(year / 100);
 }
+
+
+
 
 console.log(century(1705));
